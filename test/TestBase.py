2a9e58385be5ca9131899522a9722bfc9c3e5ef2
kjurek <kjurek123@gmail.com>
2017-04-13 10:42:27 +0200

    Added base class for unit tests

---
+++ test/TestBase.py
@@ -0,0 +1,14 @@
+import unittest
+import os
+
+
+def traverse_matlab_files(path):
+    for root, dirs, files in os.walk(path):
+        for fname in [f for f in files if f.endswith('.mat')]:
+            yield os.path.join(root, fname)
+
+
+class TestBase(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.data_path = os.path.join(os.path.dirname(__file__), 'data')
