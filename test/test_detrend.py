2a523c816a1a833334cabb2fe893be51293c190d
kjurek <kjurek123@gmail.com>
2017-04-13 15:44:38 +0200

    Fixed unit test in detrend

--- test/test_detrend.py
+++ test/test_detrend.py
@@ -26,9 +26,11 @@ class MatlabComparisonTestDetrend(TestBase):
 
     def test_detrend_from_nilearn(self):
         test_data = sio.loadmat(os.path.join(self.data_path, 'detrend', 'MyDetrend_normalize_nii.mat'))
-        result = np.transpose(signal.clean(signals=test_data['TC'], t_r=2, high_pass=0.008))
+        result = signal.clean(signals=np.transpose(test_data['TC']), t_r=1, detrend=False)
         self.assertEquals(test_data['TCN'].shape, result.shape)
+        np.testing.assert_allclose(test_data['TCN'], result, rtol=0.01)
 
+        """
         random_signal_index = random.choice(range(result.shape[0]))
         t = np.arange(0, test_data['TC'].shape[1])
         plt.xlabel('Time')
@@ -36,7 +38,8 @@ class MatlabComparisonTestDetrend(TestBase):
         plt.plot(t, test_data['TCN'][:, random_signal_index], t, result[:, random_signal_index])
         plt.legend(['MATLAB', 'nilearn.signal.clean'])
         plt.show()
-        #np.testing.assert_allclose(test_data['TCN'], result, rtol=0.01)
+        """
+
 
 if __name__ == '__main__':
     unittest.main()
