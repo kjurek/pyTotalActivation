a2e9050bb01e09e96785605dd43faf5203ff8348
Marcel Falkiewicz <mfalkiewicz@cbs.mpg.de>
2017-04-13 10:52:13 +0200

    Initial method of reading NIFTI files

--- TotalActivationTool.py
+++ TotalActivationTool.py
@@ -13,7 +13,11 @@ class Config(object):
 
 class TotalActivationTool(object):
     def __init__(self):
-        self.config = None
+        self.config = {'Detrend': True,
+                       'Standardize' : True,
+                       'Highpass' : 0.01,
+                       'Lowpass' : None,
+                       'TR' : 2}
         self.voxels = None
 
     def load(self, data_path, atlas_path, config):
@@ -23,6 +27,34 @@ class TotalActivationTool(object):
         self.data_shape = data.shape
         self.dimension = len(data.shape)
         self.voxels = data[np.nonzero(atlas * np.ndarray.sum(data, axis=self.dimension - 1))]
+        logging.debug('Dimension={}'.format(self.dimension))
+        logging.debug('Voxels.shape={}'.format(self.voxels.shape))
+
+    def load_nifti(self, d, a):
+        '''
+        Basic function to load NIFTI time-series and flatten them to 2D array
+
+        Imputs:
+        d : data (4D NIFTI file)
+        a : atlas (3D NIFTI file)
+        '''
+
+        import nibabel as nib
+        from nilearn.input_data import NiftiMasker
+
+        self.masker = NiftiMasker(mask_strategy='epi',
+                           memory="nilearn_cache", memory_level=2,
+                           standardize=self.config['Standardize'],
+                           detrend=self.config['Detrend'],
+                           high_pass = self.config['Highpass'],
+                           low_pass = self.config['Lowpass'],
+                           t_r = self.config['TR'])
+
+        self.masker.fit(data)
+        self.data = self.masker.transform(data)
+        self.data_shape = self.data.shape
+        self.dimension = len(self.data.shape)
+        self.voxels = self.data.shape[1]
         logging.debug('Dimension={}'.format(self.dimension))
         logging.debug('Voxels.shape={}'.format(self.voxels.shape))
 
