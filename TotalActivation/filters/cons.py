9a1a569376a4f9622a178a345a6e8d0bd8b9a357
kjurek <kjurek123@gmail.com>
2017-04-14 18:05:13 +0200

    Fixed ugly warning 'casting complex to real number'

--- TotalActivation/filters/cons.py
+++ TotalActivation/filters/cons.py
@@ -4,7 +4,7 @@ from itertools import combinations
 
 def cons(root):
     n = len(root)
-    fil = np.zeros(n + 1)
+    fil = np.zeros(n + 1, dtype=np.complex)
     fil[0] = 1
     for i in range(1, n + 1):
         fil[i] = np.power(-1.0, i) * np.sum(np.exp(np.sum(np.array(list(combinations(root, i))), 1)))
