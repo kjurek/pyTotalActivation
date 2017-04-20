79a2b91a37ccd521dba40b7e38542d855089f9de
Marcel Falkiewicz <mfalkiewicz@gmail.com>
2017-04-20 11:48:41 +0200

    Temporal_TA tests (#39)
    * Added unit tests for temporal_TA, left old ones commented
    
    * Removed commented tests
    
    * Added unit tests for both cost_save = False and True
    
    * Added unit tests for temporal_TA, left old ones commented
    
    * Removed commented tests
    
    * Added unit tests for both cost_save = False and True

--- TotalActivation/tests/test_TemporalTA.py
+++ TotalActivation/tests/test_TemporalTA.py
@@ -2,22 +2,19 @@ from os.path import join
 
 import numpy as np
 import os
-import scipy.io as sio
 
 from TestBase import TestBase
-from TestBase import traverse_matlab_files
-from TotalActivation.process.temporal import temporal_TA
 from TotalActivation import TotalActivation
-
 
 class MatlabComparisonTestTemporalTA(TestBase):
 
-    def test_TemporalTA(self):
+    def test_TemporalTA_nocostsave(self):
         test_ta = TotalActivation()
         test_ta.load_text_data(os.path.join(self.data_path, 'TemporalTA/input_matlab_normalized.csv'),)
         test_ta.data = test_ta.data[:,0:10]
         test_ta.n_voxels = 10
         test_ta.config['TR'] = 1
+        test_ta.config['cost_save'] = False
         test_ta._get_hrf_parameters()
         test_ta.t_iter = 2500
         test_ta._temporal()
@@ -26,3 +23,20 @@ class MatlabComparisonTestTemporalTA(TestBase):
                                         delimiter=',')[:,0:10]
 
         np.testing.assert_almost_equal(test_ta.deconvolved_, expected_output, decimal = 3)
+
+
+    def test_TemporalTA_costsave(self):
+        test_ta = TotalActivation()
+        test_ta.load_text_data(os.path.join(self.data_path, 'TemporalTA/input_matlab_normalized.csv'), )
+        test_ta.data = test_ta.data[:, 0:10]
+        test_ta.n_voxels = 10
+        test_ta.config['TR'] = 1
+        test_ta.config['cost_save'] = True
+        test_ta._get_hrf_parameters()
+        test_ta.t_iter = 2500
+        test_ta._temporal()
+
+        expected_output = np.genfromtxt(os.path.join(self.data_path, 'TemporalTA/phantom_block_nospatial.csv'),
+                                        delimiter=',')[:, 0:10]
+
+        np.testing.assert_almost_equal(test_ta.deconvolved_, expected_output, decimal=3)
